1766ab9a034260138ba6613b74450dd3695fc780
JianHui (Jake) Li <lijake2001@gmail.com>
2023-05-24 21:26:07 -0400

    update web app

--- streamlit_app/homepage.py
+++ streamlit_app/homepage.py
@@ -30,46 +30,55 @@ generate_rec = st.button('Generate Recommendations')
 if generate_rec:
     @st.cache_resource(show_spinner=False)
     def create_cos_sim(string_column):
-        tf_vec = TfidfVectorizer(ngram_range=(1,2), max_df=0.7, min_df=3)
+        tf_vec = TfidfVectorizer(ngram_range=(1,2), max_df=0.5, min_df=0.01)
         tfidf_matrix = tf_vec.fit_transform(string_column)
         tfidf_array = tfidf_matrix.toarray()
         return cosine_similarity(tfidf_array, tfidf_array)
 
     def get_recommendations(title, cosine_sim, indices):
+        # get index of the title requested
         idx = indices[title]
+        # use index to sort the row in the cosine similarity matrix and get top 10 indices
         sim_scores = list(enumerate(cosine_sim[idx]))
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
         first = 1
         last = 21
         top_20 = sim_scores[first:last]
-        movie_indices = [i[0] for i in top_20]
+        anime_indices = [i[0] for i in top_20]
+        animes_scores = [i[1] for i in top_20]
         
-        # return the top 20 most similar animes that doesn't contain the title
-        results = cleaned_df[['title', 'alternative_title(s)', 'synopsis', 'genres', 'studios', 'mean', 'year', 'status', 'medium_picture_url']].iloc[movie_indices]
+        # return the top 10 most similar animes that doesn't contain the title
+        results = cleaned_df[['title', 'alternative_title(s)', 'synopsis', 'genres', 'studios', 'mean', 'year', 'status', 'medium_picture_url']].iloc[anime_indices]
+        results['cosine_similarity_score'] = animes_scores
+        # filter out animes that have a mean (rating) lower than 5
+        filtered_results = results[results['mean']>=5]
+        # check if results contain the words (longer than 5 letters) in the title, if yes, remove the anime
+        title = re.sub('[()]', '', str(title))
+        title = list(set((title.split())))
+        title_word_list = [x for x in title if len(x)>5]
+        if not title_word_list:
+            pass
+        else:
+            for x in title_word_list:
+                filtered_results = filtered_results[filtered_results['title'].str.contains(x)==False]
         
-        # filter out animes that have a mean (rating) lower than 7
-        filtered_results = results[results['mean']>=7]
-        # check if results contain the words in the title, if yes, remove and add new recommendation until no sequels exists in the top 20
-        title_word_list = [x for x in title.split() if len(x)>5]
-        for i in title_word_list:
-            filtered_results = filtered_results[filtered_results['title'].str.contains(i)==False]
-        # filtered_results = results[results['title'].str.contains(title)==False]
-
+        # check how many rows removed/filtered out, if none then just return the results. If yes, then add new recommendation until no sequels exists in the top 10
         diff = results.shape[0] - filtered_results.shape[0]
         
         if diff == 0:
             return filtered_results
         else:
-            add_recommendations = pd.DataFrame(columns=['title', 'alternative_title(s)', 'synopsis', 'genres', 'studios', 'mean', 'year', 'status', 'medium_picture_url'])
+            add_recommendations = pd.DataFrame(columns=['title', 'alternative_title(s)', 'synopsis', 'genres', 'studios', 'mean', 'year', 'status', 'medium_picture_url', 'cosine_similarity_score'])
             while diff > 0:
                 new_idx = sim_scores[last:last+diff]
                 last = last + diff
-                new_movie_idx = [i[0] for i in new_idx]
-                new_recommendations = cleaned_df[['title', 'alternative_title(s)', 'synopsis', 'genres', 'studios', 'mean', 'year', 'status', 'medium_picture_url']].iloc[new_movie_idx]
-                filtered_new_recommendations = new_recommendations[new_recommendations['mean']>=7]
+                new_anime_idx = [i[0] for i in new_idx]
+                new_animes_scores = [i[1] for i in new_idx]
+                new_recommendations = cleaned_df[['title', 'alternative_title(s)', 'synopsis', 'genres', 'studios', 'mean', 'year', 'status', 'medium_picture_url']].iloc[new_anime_idx]
+                new_recommendations['cosine_similarity_score'] = new_animes_scores
+                filtered_new_recommendations = new_recommendations[new_recommendations['mean']>=5]
                 for i in title_word_list:
                     filtered_new_recommendations = filtered_new_recommendations[filtered_new_recommendations['title'].str.contains(i)==False]
-                # filtered_new_recommendations = filtered_new_recommendations[filtered_new_recommendations["title"].str.contains(title)==False]
 
                 add_recommendations = pd.concat([add_recommendations, filtered_new_recommendations])    
                 diff = new_recommendations.shape[0] - filtered_new_recommendations.shape[0]
